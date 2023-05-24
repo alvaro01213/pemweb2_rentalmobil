2d34792c57e673d0ed17111a5c2194dc577aa483
alvaro01213 <anandaiqbaal01@gmail.com>
2023-05-24 15:14:20 +0700

    rental mobil

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\MobilController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/welcome', function() {
+    echo "Selamat Datang di laravel";
+});
+
+Route::get('/greeting', function() {
+    return view('greeting');
+});
+
+Route::get('/', function() {
+    return view('index');
+});
+
+Route::get('/mobil',[MobilController::class,'index']); 
+Route::get('/mobil/create',[MobilController::class,'create']);
+Route::post('/mobil/simpanData',[MobilController::class,'store']);
+Route::get('/merk', [Merkcontroller::class, 'index']);
