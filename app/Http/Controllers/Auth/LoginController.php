2fcf5e55ab8a34a23378c84703a77b50fcdad317
alvaro01213 <anandaiqbaal01@gmail.com>
2023-07-03 13:29:57 +0700

    praktikum14

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+
+class LoginController extends Controller
+{
+    //memanggil form login
+    function index()
+    {
+        return view('pages.auth.login');
+    }
+    function login(Request $request)
+    {
+        //validasi user
+        $validatedLogin = $request->validate([
+            'email' => 'required',
+            'password' =>'required',
+        ]);
+        //proses login
+        if(Auth::attempt($validatedLogin)){
+            return redirect()->to('/merk');
+        }else{
+            return redirect()->back();
+        }
+        
+    }
+    function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->to('/login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\MobilController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,22 +21,20 @@ Route::get('/welcome', function() {
     echo "Selamat Datang di laravel";
 });
 
+
+
 Route::get('/greeting', function() {
     return view('greeting');
 });
 
-Route::get('/', function() {
-    return view('index');
-});
-
-Route::get('/mobil',[MobilController::class,'index']); 
-Route::get('/mobil/create',[MobilController::class,'create']);
-Route::post('/mobil/simpanData',[MobilController::class,'store']);
-
+Route::middleware(['auth'])->group(function(){ 
+Route::get('/', function() {return view('index');});
+    
+    
+    
 Route::get('/merk', [Merkcontroller::class, 'index']);
 Route::get('/merk/create', [Merkcontroller::class, 'create']);
 Route::post('/merk/simpanData', [Merkcontroller::class, 'store']);
-
 Route::get('/merk/edit/{id}', [Merkcontroller::class, 'edit']);
 Route::post('/merk/update/{id}', [Merkcontroller::class, 'update']);
 Route::get('/merk/delete/{id}', [Merkcontroller::class, 'delete']);
@@ -54,3 +53,23 @@ Route::post('/mobil/simpan-data',[MobilController::class,'store']); //mengirim r
 Route::get('/mobil/delete/{id}',[MobilController::class,'delete']);
 Route::post('/mobil/update/{id}',[MobilController::class,'update']);
 Route::get('/mobil/edit/{id}',[MobilController::class,'edit']);
+
+Route::get('/mobil',[MobilController::class,'index']);
+Route::get('/mobil/create',[MobilController::class,'create']); //memanggil form
+Route::post('/mobil/simpan-data',[MobilController::class,'store']); //mengirim request
+Route::get('/mobil/delete/{id}',[MobilController::class,'delete']);
+Route::post('/mobil/update/{id}',[MobilController::class,'update']);
+Route::get('/mobil/edit/{id}',[MobilController::class,'edit']);
+
+
+Route::get('/logout',[Auth\LoginController::class,'Logout']);
+    
+});
+
+
+
+
+Route::get('/login',[Auth\LoginController::class,'index'])->name('Login');
+Route::post('/login/proses',[Auth\LoginController::class,'login']);
+Route::get('/register',[Auth\RegisterController::class,'index']);
+Route::post('/register/proses',[Auth\RegisterController::class,'register']);
